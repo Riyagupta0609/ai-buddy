8bebc62eafff47596d71f3c097cf79d2ef6a0e23
Riyagupta0609 <riyagupta8834@gmail.com>
2026-07-03 01:33:10 +0530

    Update app.py
    Updated API key configuration and improved user input handling.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import google.generativeai as genai
 
 # Configure Gemini API
-genai.configure(api_key="YOUR_API")
+genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 
 # Load Gemini model
 model = genai.GenerativeModel("gemini-2.5-flash")
